e9ac684bc6677cd4097bc4702514910d54fddd7a
AkhilThomas4936 <thomas.del011@gmail.com>
2020-10-19 00:51:46 -0400

    line chart test successful

--- src/components/SvgExample.js
+++ src/components/SvgExample.js
@@ -5,32 +5,44 @@ export default function SvgExample({dailyData}) {
     const svgRef = useRef()
    
     useEffect(()=>{
-        const xMinMax = extent(dailyData, d=>d.date)
+        const xMinMax = extent(dailyData, d=>new Date(d.date))
         console.log(xMinMax)
 
         const xScale = scaleTime()
             .domain(xMinMax)
-            .range([0,500])
-        console.log(xScale(new Date ('2020/1/22')))
+            .range([100,800])
+        console.log(xScale(new Date('2020-03-28')))
 
         const yMinMax = extent(dailyData,d=>d.confirmed)
         console.log(yMinMax)
 
         const yScale = scaleLinear()
             .domain(yMinMax)
-            .range([0,500])
+            .range([100,600])
         // console.log(yScale(555))
 
         const lineData = dailyData.map(d=>{
             return {
-                x:xScale(d.date),
-                y:yScale(d.confirmed)
+                x:xScale(new Date (d.date)),
+                y:yScale( d.confirmed)
                 
             }
         })
         console.log(lineData)
 
-        // const svg = select(svgRef.current)
+        const myLine = line()
+            .x(value=>value.x)
+            .y(value=>600-value.y)
+
+
+        const svg = select(svgRef.current)
+        svg.selectAll('path')
+        .data([lineData])
+        .join('path')
+        .attr('d',value=>myLine(value))
+        .attr('fill','none')
+        .attr('stroke','blue')
+        .attr('stroke-width',4)
 
        
 
@@ -40,7 +52,7 @@ export default function SvgExample({dailyData}) {
     
     return (
         <div>
-            <svg height='500' width='500' ref={svgRef}>
+            <svg height='600' width='800' ref={svgRef}>
                
 
             </svg>
