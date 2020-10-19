9bb3c0f23d8eef6f62e0fb4377457bce50a8f26e
AkhilThomas4936 <thomas.del011@gmail.com>
2020-10-19 00:00:38 -0400

    scaleTime

--- src/components/SvgExample.js
+++ src/components/SvgExample.js
@@ -1,22 +1,49 @@
 import React,{useEffect,useRef} from 'react'
-import {select } from 'd3'
+import {select,extent,scaleTime,scaleLinear, selectAll ,line } from 'd3'
 
 export default function SvgExample({dailyData}) {
     const svgRef = useRef()
    
     useEffect(()=>{
-        const svg = select(svgRef.current)
-        svg.selectAll('circle').data(dailyData)
-        .join('circle')
-        .attr('r',50)
-        .attr('cx',(d,i)=>i)
-        .attr('cy',(d,i)=>i+20)
-        .attr('stroke','blue')
-        .attr('fill','red')
-    },[dailyData])
+        const xMinMax = extent(dailyData, d=>d.date)
+        console.log(xMinMax)
+
+        const xScale = scaleTime()
+            .domain(xMinMax)
+            .range([0,500])
+        console.log(xScale(new Date ('2020/1/22')))
+
+        const yMinMax = extent(dailyData,d=>d.confirmed)
+        console.log(yMinMax)
+
+        const yScale = scaleLinear()
+            .domain(yMinMax)
+            .range([0,500])
+        // console.log(yScale(555))
+
+        const lineData = dailyData.map(d=>{
+            return {
+                x:xScale(d.date),
+                y:yScale(d.confirmed)
+                
+            }
+        })
+        console.log(lineData)
+
+        // const svg = select(svgRef.current)
+
+       
+
+
+    },[dailyData,])
+
+    
     return (
         <div>
-            <svg height='5000' width='5000' ref={svgRef}></svg>
+            <svg height='500' width='500' ref={svgRef}>
+               
+
+            </svg>
             
         </div>
     )
--- src/store/actions/world.js
+++ src/store/actions/world.js
@@ -5,7 +5,7 @@ export const GET_WORLD_DATA ="GET_WORLD_DATA"
 //Bringing the worldwide data
 export const getWorldData=()=> async dispatch=>{
     try{
-        const res = await axios.get("https://covid19.mathdro.id/api/daily")
+        const res = await axios.get("https://covid19.mathdro.id/api/daily/")
         const data = res.data.map(dailyData=>{
             return{
                 confirmed:dailyData.totalConfirmed,
